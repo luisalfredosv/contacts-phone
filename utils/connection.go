81c28ac017059af6d993f85c4569ac206b52df6c
luisalfredosv <luissarabia80@gmail.com>
2021-09-07 11:40:08 -0400

    [Set] env db access connection.go

--- utils/connection.go
+++ utils/connection.go
@@ -1,13 +1,20 @@
 package utils
 
 import (
-	"log"
+    "log"
+    "os"
 	"github.com/jinzhu/gorm"
-	"github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-func getConnection() *gorm.DB {
-	db, err := gorm.Open("mysql", "userDb:pwdDb@/dbName?charset=utf8&parseTime=True&loc=Local")
+func GetConnection() *gorm.DB {
+	
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbDatabase := os.Getenv("DB_DATABASE")
+
+	db, err := gorm.Open(dbDriver, dbUser+":"+dbPassword+"@/"+dbDatabase+"?charset=utf8&parseTime=True&loc=Local")
 
 	if err != nil {
 		log.Fatal(err)
