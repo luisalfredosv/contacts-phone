03a0e4ab9d9b1fbc1d4cb79577a9f5d4180343e5
luisalfredosv <luissarabia80@gmail.com>
2021-09-07 05:07:03 -0400

    [Fix] import

--- models/contact.go
+++ models/contact.go
@@ -4,13 +4,13 @@ import "github.com/jinzhu/gorm"
 
 type Contact struct {
 
-	grom.Model
+	gorm.Model
 	
 	Name string `json:"name"`
 	Age uint `json:"age"`
 	phone string `json:"phone" gorm:"size:20"`
 	address string `json:"address"`
 	email string `json:"email"`
-	description string `json:"description" grom:"type:TEXT"`
+	description string `json:"description" gorm:"type:TEXT"`
 
 }
