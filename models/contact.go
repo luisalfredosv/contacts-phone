3a4a6cb12fca6ea0692ac2c7dd2e770676d833ef
luisalfredosv <luissarabia80@gmail.com>
2021-09-07 15:55:35 -0400

    [Implement validation] contact.go

--- models/contact.go
+++ models/contact.go
@@ -1,13 +1,15 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+)
 
 type Contact struct {
 
 	gorm.Model
 	
-	Name string `json:"name"`
-	Age uint `json:"age"`
+	Name string `json:"name" validate:"required,min=4,max=15"`
+	Age uint `json:"age" validate:"required,min=10,max=120"`
 	Phone string `json:"phone" gorm:"size:20"`
 	Address string `json:"address"`
 	Email string `json:"email"`
