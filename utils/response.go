c521727475c37c3b8ffdf95e5192a488e5498253
luisalfredosv <luissarabia80@gmail.com>
2021-09-07 11:39:37 -0400

    [Update] response.go

--- utils/response.go
+++ utils/response.go
@@ -3,19 +3,14 @@ package utils
 import "net/http"
 
 func SendResponse(w http.ResponseWriter, status int, data []byte){
-	w.Header().set("Content-Type", "application/json")
-
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-
-	w.Wwrite(data)
+	w.Write(data)
 }
 
-func SendErr(w http.ResponseWriter, status int){
-	data:[]byte(`{}`)
-
-	w.Header().set("Content-Type", "application/json")
-
+func SendErr(w http.ResponseWriter, status int) {
+	data := []byte(`{}`)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-
-	w.Wwrite(data)
+	w.Write(data)
 }
