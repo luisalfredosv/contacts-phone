5481affd2f795c5c7071402fe8d38aaec695960c
luisalfredosv <luissarabia80@gmail.com>
2021-09-07 11:39:45 -0400

    [Update] migrations.go

--- utils/migrations.go
+++ utils/migrations.go
@@ -2,16 +2,14 @@ package utils
 
 import (
 	"fmt"
-	// "github.com/chejo343/go_contacts/models"
 	"github.com/luisalfredosv/golang-gorilla/models"
 )
 
 func MigrateDB(){
 	db := GetConnection()
-
 	defer db.Close()
-
-	fmt.Println("Migrating models...")
 	
+	fmt.Println("Migrating models...")
+
 	db.AutoMigrate(&models.Contact{})
 }
