42521fbb2edf35f5415a98ec983f421b8f4bce34
luisalfredosv <luissarabia80@gmail.com>
2021-09-08 11:42:31 -0400

    [Update] response.go

--- utils/response.go
+++ utils/response.go
@@ -7,10 +7,3 @@ func SendResponse(w http.ResponseWriter, status int, data []byte){
 	w.WriteHeader(status)
 	w.Write(data)
 }
-
-func SendErr(w http.ResponseWriter, status int) {
-	data := []byte(`{}`)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	w.Write(data)
-}
